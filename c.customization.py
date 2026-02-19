6da950674359d2a0b22bde5a924353b474afe8f9
Shubham Baidhya <shubhambaidhya@Shubhams-MacBook-Air.local>
2026-02-19 19:32:40 +0545

    Customization

--- c.customization.py
+++ c.customization.py
@@ -2,8 +2,8 @@
 
 import matplotlib.pyplot as plt
 
-year = [1950, 1951, 1952, 1953,1954,1955,1956]
-pop= [2.538, 2.57, 2.62, 2.85,2.95,2.98,3.15]
+year = [1950, 1951, 1952, 1953,1954,1955]
+pop= [2.538, 2.57, 2.62, 2.85,2.95,2.98]
 
 
 plt.plot(year, pop)
@@ -12,8 +12,6 @@ plt.plot(year, pop)
 plt.xlabel('Year')
 plt.ylabel('Population')
 plt.title('World Population Projection')
-plt.yticks([0,2,3,6,8,10,12],
-            ['0','2B','4B','6B','8B','10B','12B'])
-
-
+plt.yticks([0,2,3,6,8,10],
+            ['0','2B','4B','6B','8B','10B',])
 plt.show()
